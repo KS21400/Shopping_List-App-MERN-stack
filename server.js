36094b4545b1d43c650097ff5217ec246dc12922
ks21400 <21keshav007@gmail.com>
2020-07-13 18:33:25 +0530

    full stack with redux

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-const items = require('./routes/api/items');
-
+const config = require('config');
 const app = express();
+var bodyParser = require('body-parser')
+
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+
 
 // DB Config
-const db = require('./config').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
-  .connect(db)
+  .connect(db, {
+    useNewUrlParser : true,
+    useCreateIndex : true
+  })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 
 const port = process.env.PORT || 5000;
 
